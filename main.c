f2166a36216e5cbaa44c84424ed612794b8fc41c
Leftzara <nicolas.zarate.m@mail.pucv.cl>
2021-07-05 16:07:52 -0400

    Codigo Comidas y leves cambios

--- main.c
+++ main.c
@@ -180,6 +180,30 @@ void Cargar(HashMap * mapEjercicios, HashMap * mapTipos){
     fclose(file);
 }
 
+void cargarComida(HashMap* mapComida){
+    FILE *archivo;
+    archivo = fopen("Alimentos.csv", "r" );
+    if(!archivo)return;
+    char *name, *aux;
+    int kc, fats, carbs, prots, i;
+    char linea[100];
+    fgets(linea,99,archivo);
+    while(fgets(linea, 99, archivo) != NULL){
+        for(i=0;i<5;i++){
+            aux = get_csv_field(linea,i);
+            if(i==0)name=aux;
+            if(i==1)kc=atol(aux);
+            if(i==2)fats=atol(aux);
+            if(i==3)carbs=atol(aux);
+            if(i==4)prots=atol(aux);
+        }
+        Comida* Alimento;
+        Alimento = createComida(name,kc,fats,carbs,prots);
+        insertMap(mapComida,name,Alimento);
+    }
+    fclose(archivo);
+}
+
 //Calcula el IMC del usuario
 void CalcularIMC(Stack *HistorialI){
 
@@ -444,29 +468,6 @@ void modificarRutina(List* Rutinas){
     }
 }
 
-void mostrarRutinas(List* Rutinas){
-    Rutina* aux = first(Rutinas);
-    while (aux){
-        detalleRutina(aux);
-        aux=next(Rutinas);
-    }
-    int caso=1;
-    printf("--------------------------------------------------------------------\n");
-    while(caso!=0){
-        printf("1. Modificar rutina\n");
-        printf("2. Eliminar rutina\n");
-        printf(" 0. Volver al menu principal\n");
-        scanf("%d", &caso);
-
-        switch(caso){
-            case 1:modificarRutina(Rutinas);break;
-            case 2:borrarRutina(Rutinas);break;
-            
-        }
-    }
-    printf("--------------------------------------------------------------------\n");
-}
-
 void detalleRutina(Rutina *r)
 {
     int cont=0;
@@ -492,37 +493,30 @@ void detalleRutina(Rutina *r)
     }
 }
 
-void cargarComida(HashMap* mapComida){
-    FILE *archivo;
-    archivo = fopen("Alimentos.csv", "r" );
-    char linea[100];
-    fgets(linea,99,archivo);
+void mostrarRutinas(List* Rutinas){
+    Rutina* aux = first(Rutinas);
+    while (aux){
+        detalleRutina(aux);
+        aux=next(Rutinas);
+    }
+    int caso=1;
+    printf("--------------------------------------------------------------------\n");
+    while(caso!=0){
+        printf("1. Modificar rutina\n");
+        printf("2. Eliminar rutina\n");
+        printf(" 0. Volver al menu principal\n");
+        scanf("%d", &caso);
 
-    while(fgets(linea,100,archivo)){
-        char* name;
-        long kc, fats, carbs, prots;
-        for(int i=0;i<5;i++){
-            char* aux = get_csv_field(linea,i);
-            if(i==0)name=aux;
-            if(i==1)kc=conversorInt(aux);
-            if(i==2)fats=conversorInt(aux);
-            if(i==3)carbs=conversorInt(aux);
-            if(i==4)prots=conversorInt(aux);
-        }
-        Comida* Alimento= searchMap(mapComida,name);
-        if(!Alimento){
-            Alimento= createComida(name,kc,fats,carbs,prots);
-            insertMap(mapComida,name,Alimento);
+        switch(caso){
+            case 1:modificarRutina(Rutinas);break;
+            case 2:borrarRutina(Rutinas);break;
+            
         }
     }
-    fclose(archivo);
+    printf("--------------------------------------------------------------------\n");
 }
 
-void menuComida(HashMap* mapComida){
-    printf("En proceso de ser diseñada\n");
-}
-
-void realizaRutina(Stack *stackIMC, Stack *stackH, List *rutinas)
+void realizarRutina(Stack *stackIMC, Stack *stackH, List *rutinas)
 {
     int cont =0, i,op=2;
     CalcularIMC(stackIMC);
@@ -560,17 +554,138 @@ void realizaRutina(Stack *stackIMC, Stack *stackH, List *rutinas)
     
 }
 
+void mostrarDieta(Stack* s){
+    Stack* s2=createStack();
+    Comida* a;
+    if(get_size(s)==0){
+        printf("No ha agregado nada a su dieta de hoy.\n");
+        return;
+    }
+    while(get_size(s)!=0){
+        pushStack(s2,topStack(s));
+        popStack(s);  
+    }
+    printf("Su dieta de hoy a sido: \n");
+    while(get_size(s2)!=0){
+        a=topStack(s2);
+        printf("%s: ",a->nombre);
+        printf("%ldkc G=%ldmg C=%ldmg P=%ldmg\n",a->calorias,a->grasas,a->carbohidratos,a->proteinas);
+        pushStack(s,topStack(s2));
+        popStack(s2);
+    }
+}
+
+void crearcc(char nombre[50], HashMap* map, Stack* s){
+    Comida* Alimento;
+    int kc, fats, carbs, prot;
+    printf("Favor introducir las calorias del alimento\n");
+    scanf("%d",&kc);
+    printf("Favor introduzca los miligramos de grasas del alimento\n");
+    scanf("%d",&fats);
+    printf("Favor intrduzca los miligramos de carbohidratos del alimento\n");
+    scanf("%d",&carbs);
+    printf("Favor introduzca los miligramos de proteinas del alimento\n");
+    scanf("%d",&prot);
+    Alimento = createComida(nombre,kc,fats,carbs,prot);
+    insertMap(map,nombre,Alimento);
+    pushStack(s,Alimento);
+}
+
+void crearcc2(HashMap* map){
+    getchar();
+    Comida* Alimento;
+    Comida* Alimento2;
+    char nombre[50];
+    long kc, fats, carbs, prot;
+    int i=0;
+    printf("Favor ingrese el nombre del alimento que quiere crear\n");
+    scanf("%[^\n]",nombre);
+    Alimento2=searchMap(map,nombre);
+    if(Alimento2){
+        printf("El alimento ya existe, desea remplazarlo?\n 1. Si\n 2. No\n");
+        scanf("%d",&i);
+        if(i==1){
+            printf("Favor introducir las calorias del alimento\n");
+            scanf("%ld",&Alimento2->calorias);
+            printf("Favor introduzca los miligramos de grasas del alimento\n");
+            scanf("%ld",&Alimento2->grasas);
+            printf("Favor intrduzca los miligramos de carbohidratos del alimento\n");
+            scanf("%ld",&Alimento2->carbohidratos);
+            printf("Favor introduzca los miligramos de proteinas del alimento\n");
+            scanf("%ld",&Alimento2->proteinas);
+            return;
+        }else return;
+    }
+    printf("Favor introducir las calorias del alimento\n");
+    scanf("%ld",&kc);
+    printf("Favor introduzca los miligramos de grasas del alimento\n");
+    scanf("%ld",&fats);
+    printf("Favor intrduzca los miligramos de carbohidratos del alimento\n");
+    scanf("%ld",&carbs);
+    printf("Favor introduzca los miligramos de proteinas del alimento\n");
+    scanf("%ld",&prot);
+    Alimento = createComida(nombre,kc,fats,carbs,prot);
+    insertMap(map,nombre,Alimento);
+}
+
+
+void anadirComida(Stack* s, HashMap* map){
+    Comida* Alimento;
+    int x;
+    getchar();
+    char nombre[50];
+    printf("Favor ingrese el nombre del alimento que desea añadir a su dieta de hoy\n");
+    scanf("%[^\n]",nombre);
+    //fgets(nombre, 50, stdin);
+    Alimento = searchMap(map, nombre);
+    if(!Alimento){
+        printf("El alimento que busco no existe.\n ¿Desea crearlo?\n");
+        printf(" 1. Continuar\n");
+        printf(" 2. Salir\n");
+        scanf("%d",&x);
+        switch(x){
+            case 1:crearcc(nombre, map, s);break;
+            case 2:return;
+        }
+    }else{
+        pushStack(s, Alimento);
+    }
+}
+
+void menuComida(HashMap* mapComida, Stack* Dieta){
+    int caso=2;;
+    //printf("Funcion en proceso de implementacion\n");
+    printf("-----------------------------------------------------------------------\n");
+    printf("                            Menu de Comida\n");
+    printf("-----------------------------------------------------------------------\n");
+    while(caso!=0){
+        printf("Favor ingrese la accion que quiere ejercer\n");
+        printf(" 1. Añadir Comida\n");
+        printf(" 2. Ver dieta de hoy\n");
+        printf(" 3. Crear alimento\n");
+        printf(" 0. Salir\n");
+        scanf("%d",&caso);
+        switch(caso){
+            case 1:anadirComida(Dieta, mapComida);break;
+            case 2:mostrarDieta(Dieta);break;
+            case 3:crearcc2(mapComida);break;
+        }
+    }
+}
+
 int main(){
     HashMap* Ejercicios=createMap(100);
     HashMap* Tipos=createMap(10);
     HashMap* Comidas=createMap(100);
     int masaCorporal = 0;
     Stack *stackIMC = createStack();
+    Stack *Dieta = createStack();
     Stack *stackH = createStack();
     List* Rutinas=createList();
     
     int caso=2;
     Cargar(Ejercicios,Tipos);
+    cargarComida(Comidas);
     printf("-----------------------------------------------------------------------\n");
     printf("                            TRAININGSAPP                             \n");
     printf("-----------------------------------------------------------------------\n");
@@ -602,7 +717,7 @@ int main(){
             case 4:HistorialRutinas(stackH);
             case 5:break;
             case 6:break;
-            case 7:menuComida(Comidas);break;
+            case 7:menuComida(Comidas,Dieta);break;
             case 8:desempenoEntrenamiento(stackIMC);break;
         }
     }
